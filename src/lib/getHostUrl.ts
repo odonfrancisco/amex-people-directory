22a6222c38d9efb8c215726bc27f9458143f62db
Francisco Odon <francisco.odon11@gmail.com>
2025-03-20 10:52:52 -0600

    Ensure app works when built, not just in dev env

--- src/lib/getHostUrl.ts
+++ src/lib/getHostUrl.ts
@@ -2,6 +2,8 @@ import { IncomingMessage } from 'http'
 
 export function makeHostUrl(req: IncomingMessage) {
   return `${
-    process.env.NODE_ENV === 'production' ? `https://${req.headers.host}` : 'http://127.0.0.1:3000'
+    process.env.NODE_ENV === 'production' && !req.headers.host?.includes('localhost')
+      ? `https://${req.headers.host}`
+      : 'http://127.0.0.1:3000'
   }`
 }
